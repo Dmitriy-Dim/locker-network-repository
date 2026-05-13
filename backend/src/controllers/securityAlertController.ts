270e82845834856110de419c5a3f213fc97bf282
DmitriiBank <dm.skakov@gmail.com>
2026-05-13 17:43:21 +0300

    add security routes for operator

--- backend/src/controllers/securityAlertController.ts
+++ backend/src/controllers/securityAlertController.ts
@@ -1,14 +1,6 @@
-import { NextFunction, Request, Response } from "express";
+import {NextFunction, Request, Response} from "express";
 
-import { SecurityAlertService } from "../services/SecurityAlertService";
-
-export const getStoredAlerts = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        return await SecurityAlertService.getStoredAlerts(req, res);
-    } catch (e) {
-        next(e);
-    }
-};
+import {SecurityAlertService} from "../services/SecurityAlertService";
 
 export const queryCloudWatchAlerts = async (req: Request, res: Response, next: NextFunction) => {
     try {
